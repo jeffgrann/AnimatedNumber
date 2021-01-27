3cdf087149df6a1e5972ff682df87ee5e9b80906
jeffgrann <jeff@successware.net>
2021-01-26 19:35:16 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AnimatedNumber
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import AnimatedNumberTests
+
+var tests = [XCTestCaseEntry]()
+tests += AnimatedNumberTests.allTests()
+XCTMain(tests)
